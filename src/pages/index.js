c6775476690ce8a4bff1c0475528f5e431cb29a4
Marco Medeiros <marcomedeirosfilho@gmail.com>
2023-10-22 12:59:20 +0100

    Adjust layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,10 +14,10 @@ const IndexPage = () => {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
       <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&display=swap" rel="stylesheet"></link>
       <Header setView={setView}/>
-      { true && <Hero /> }
-      { true && <About /> }
-      { true && <Contact /> }
-      { true && <Portfolio /> }
+      { (view === 'hero') && <Hero /> }
+      { (view === 'about') && <About /> }
+      { (view === 'contact') && <Contact /> }
+      { (view === 'portfolio') && <Portfolio /> }
     </>
     )
 }
