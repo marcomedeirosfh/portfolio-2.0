4b5eb5f3340e5061b6e0c6e33869b78d6376dd3d
Marco Medeiros <marcomedeirosfilho@gmail.com>
2023-10-23 12:58:32 +0100

    Sticky header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import React from "react";
 
 const Header = () => {
     return (
-        <header class="bg-dark-blue border-b border-green text-light-blue">
-            <nav class="hidden w-4/5 mx-auto md:flex flex-row justify-between items-center">
-                <h1 class="my-5 text-4xl cursor-pointer hover:text-white duration-300">Marco Medeiros</h1>
-                <a href="#contact" class="cursor-pointer bg-green text-2xl py-6 px-8 text-dark-blue font-bold text hover:bg-green-focus duration-300">CONTACT</a>
+        <header class="sticky top-0 bg-dark-blue border-b border-green text-light-blue z-10 ">
+            <nav class="w-4/5 mx-auto flex flex-row justify-between items-center">
+                <a href="#hero"><h1 class="my-5 text-2xl md:text-4xl cursor-pointer hover:text-white duration-300">Marco Medeiros</h1></a>
+                <a href="#contact" class="cursor-pointer bg-green text-xl md:text-2xl py-3 md:py-6 px-4 md:px-8 text-dark-blue font-bold text hover:bg-green-focus duration-300">CONTACT</a>
             </nav>
         </header>
         
